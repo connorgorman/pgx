ea96da40b9970e4804075635208282564e856e24
Connor Gorman <cgorman@redhat.com>
2023-02-02 11:26:38 -0800

    Print if context has no deadline

--- pgxpool/conn.go
+++ pgxpool/conn.go
@@ -2,6 +2,7 @@ package pgxpool
 
 import (
 	"context"
+	"fmt"
 	"sync/atomic"
 
 	"github.com/jackc/pgconn"
@@ -79,49 +80,66 @@ func (c *Conn) Hijack() *pgx.Conn {
 	return conn
 }
 
+func deadlineCheck(ctx context.Context, sql string) {
+	if _, ok := ctx.Deadline(); !ok {
+		fmt.Println("No deadline for query", sql)
+	}
+}
+
 func (c *Conn) Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error) {
+	deadlineCheck(ctx, sql)
 	return c.Conn().Exec(ctx, sql, arguments...)
 }
 
 func (c *Conn) Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error) {
+	deadlineCheck(ctx, sql)
 	return c.Conn().Query(ctx, sql, args...)
 }
 
 func (c *Conn) QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row {
+	deadlineCheck(ctx, sql)
 	return c.Conn().QueryRow(ctx, sql, args...)
 }
 
 func (c *Conn) QueryFunc(ctx context.Context, sql string, args []interface{}, scans []interface{}, f func(pgx.QueryFuncRow) error) (pgconn.CommandTag, error) {
+	deadlineCheck(ctx, sql)
 	return c.Conn().QueryFunc(ctx, sql, args, scans, f)
 }
 
 func (c *Conn) SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults {
+	deadlineCheck(ctx, "some batch query")
 	return c.Conn().SendBatch(ctx, b)
 }
 
 func (c *Conn) CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error) {
+	deadlineCheck(ctx, "copy from"+tableName.Sanitize())
 	return c.Conn().CopyFrom(ctx, tableName, columnNames, rowSrc)
 }
 
 // Begin starts a transaction block from the *Conn without explicitly setting a transaction mode (see BeginTx with TxOptions if transaction mode is required).
 func (c *Conn) Begin(ctx context.Context) (pgx.Tx, error) {
+	deadlineCheck(ctx, "begin call")
 	return c.Conn().Begin(ctx)
 }
 
 // BeginTx starts a transaction block from the *Conn with txOptions determining the transaction mode.
 func (c *Conn) BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error) {
+	deadlineCheck(ctx, "begin tx")
 	return c.Conn().BeginTx(ctx, txOptions)
 }
 
 func (c *Conn) BeginFunc(ctx context.Context, f func(pgx.Tx) error) error {
+	deadlineCheck(ctx, "begin func")
 	return c.Conn().BeginFunc(ctx, f)
 }
 
 func (c *Conn) BeginTxFunc(ctx context.Context, txOptions pgx.TxOptions, f func(pgx.Tx) error) error {
+	deadlineCheck(ctx, "begin tx func")
 	return c.Conn().BeginTxFunc(ctx, txOptions, f)
 }
 
 func (c *Conn) Ping(ctx context.Context) error {
+	deadlineCheck(ctx, "ping")
 	return c.Conn().Ping(ctx)
 }
 
